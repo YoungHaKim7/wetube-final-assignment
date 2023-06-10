ca240268fd5000ec258442fe6649168425bcf94c
YoungHa Kim <ytok1108@aol.com>
2023-06-10 21:24:00 +0900

    AWS

--- src/middlewares.js
+++ src/middlewares.js
@@ -1,4 +1,22 @@
 import multer from "multer";
+import multerS3 from "multer-s3";
+import { S3Client } from "@aws-sdk/client-s3";
+
+const s3 = new S3Client({
+  region: "ap-us-east-1",
+  // region: "us-east-1",
+  credentials: {
+    accessKeyId: process.env.AWS_ID,
+    secretAccessKey: process.env.AWS_SECRET,
+  },
+});
+
+const multerUploader = multer({
+  storage: multerS3({
+    s3: s3,
+    bucket: "wetubeeeeeeeeeeee",
+  }),
+});
 
 export const localsMiddleware = (req, res, next) => {
   res.locals.loggedIn = Boolean(req.session.loggedIn);
@@ -30,10 +48,13 @@ export const avatarUpload = multer({
   limits: {
     fileSize: 3000000,
   },
+  storage: multerUploader,
 });
+
 export const videoUpload = multer({
   dest: "uploads/videos/",
   limits: {
     fileSize: 10000000,
   },
+  storage: multerUploader,
 });
